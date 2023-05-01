2f58fe84370b64d7e19f4d6d6db817925fd8a40a
Miguel Maza <anmac_tec@outlook.com>
2023-05-01 09:49:12 -0500

    Update gitignore and lazy.lua

--- lua/user/plugins/lazy.lua
+++ lua/user/plugins/lazy.lua
@@ -14,6 +14,15 @@ vim.opt.rtp:prepend(lazypath)
 local plugins = {
   "nvim-lua/plenary.nvim",
   "folke/neodev.nvim",
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
 }
 
 local opts = {
