c9c02c791953f0b26629c9a71a768072e1c99701
Miguel Maza <anmac_tec@outlook.com>
2023-05-05 18:50:02 -0500

    feat: Add dressing to better vim.ui interfaces

--- lua/user/plugins/lazy.lua
+++ lua/user/plugins/lazy.lua
@@ -48,7 +48,7 @@ local plugins = {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
     dependencies = {
-      "nvim-tree/nvim-web-devicons",
+      { "nvim-tree/nvim-web-devicons", lazy = true },
       "echasnovski/mini.bufremove",
     }
   },
@@ -61,6 +61,22 @@ local plugins = {
     event = { "BufReadPost", "BufNewFile" },
   },
   { "NvChad/nvim-colorizer.lua" },
+  {
+    "stevearc/dressing.nvim",
+    lazy = true,
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.input(...)
+      end
+    end,
+  },
 
   -- Git
   {
