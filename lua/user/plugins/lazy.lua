98656370372897164634ed509407d17f5381104e
Miguel Maza <anmac_tec@outlook.com>
2023-05-11 00:41:29 -0500

    feat(lazy): add cmp plugin and dependencies (luasnip, sources, etc)

--- lua/user/plugins/lazy.lua
+++ lua/user/plugins/lazy.lua
@@ -48,6 +48,25 @@ local plugins = {
     },
   },
 
+  -- Completion
+  {
+    "hrsh7th/nvim-cmp",
+    version = false,
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "saadparwaiz1/cmp_luasnip",
+      {
+        "L3MON4D3/LuaSnip",
+        build = "make install_jsregexp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+      },
+    },
+  },
+
   -- UI
   {
     "nvim-tree/nvim-tree.lua",
