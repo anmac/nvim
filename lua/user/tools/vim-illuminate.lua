f36323b15d97c0f7be661d4c73d8c18a215f47a0
Miguel Maza <anmac_tec@outlook.com>
2023-05-12 00:46:52 -0500

    feat(illuminate): add config file, seat delay and denylist by file and mode

---
+++ lua/user/tools/vim-illuminate.lua
@@ -0,0 +1,41 @@
+local status_ok, illuminate = pcall(require, "illuminate")
+if not status_ok then
+  vim.notify("illuminate plugin failed!")
+  return
+end
+
+illuminate.configure({
+  providers = {
+    "lsp",
+    "treesitter",
+    "regex",
+  },
+  delay = 90,
+  filetypes_denylist = {
+    "alpha",
+    "dapui_scopes",
+    "dapui_breakpoints",
+    "dapui_stacks",
+    "dapui_watches",
+    "dap-repl",
+    "dap-terminal",
+    "dapui_console",
+    "dashboard",
+    "dirvish",
+    "fugitive",
+    "help",
+    "lazy",
+    "Man",
+    "man",
+    "Markdown",
+    "NvimTree",
+    "packer",
+    "startify",
+    "toggleterm",
+    "TelescopePrompt",
+    "",
+  },
+  modes_denylist = { "nt", "v", "vs", "V", "Vs", "CTRL-V", "CTRL-Vs", "CTRL-S", "R", "c", "!", "t", "x" },
+  under_cursor = true,
+  min_count_to_highlight = 1,
+})
