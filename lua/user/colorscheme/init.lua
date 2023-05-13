0011392b25b2e90d36094915832dea1737bdbb52
Miguel Maza <anmac_tec@outlook.com>
2023-05-12 21:12:22 -0500

    refactor(colorscheme): change vim.cmd[[]] standar for API method

--- lua/user/colorscheme/init.lua
+++ lua/user/colorscheme/init.lua
@@ -5,10 +5,10 @@ if not config_theme then
   vim.notify(colorscheme .. ": config file not found!")
 end
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+local status_ok, _ = pcall(vim.cmd.colorscheme, colorscheme)
 if not status_ok then
   vim.notify(colorscheme .. " colorscheme not found!")
-  vim.cmd([[colorscheme habamax]])
+  vim.cmd.colorscheme("habamax")
   vim.notify("loading default colorscheme...")
   return
 end
