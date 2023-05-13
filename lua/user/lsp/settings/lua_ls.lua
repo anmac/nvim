088f68565e4c144354a6ed2293c1b54feaa074f5
Miguel Maza <anmac_tec@outlook.com>
2023-05-12 20:36:25 -0500

    feat(lua_ls): add libraries to workspace

--- lua/user/lsp/settings/lua_ls.lua
+++ lua/user/lsp/settings/lua_ls.lua
@@ -4,17 +4,25 @@ return {
       version = "LuaJIT",
     },
     diagnostics = {
+      enable = true,
       globals = { "vim" },
     },
     workspace = {
       checkThirdParty = false,
-      library = vim.api.nvim_get_runtime_file("", true),
+      library = {
+        vim.api.nvim_get_runtime_file("", true),
+        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+        [vim.fn.stdpath("config") .. "/lua"] = true,
+      },
     },
     telemetry = {
       enable = false,
     },
     format = {
       enable = false,
+    },
+    single_file_support = {
+      enable = true,
     },
     hint = {
       enable = true,
