dce61633d29a3a916ca1d2a355c151fe2f6e3fc5
Miguel Maza <anmac_tec@outlook.com>
2023-06-29 18:59:39 -0500

    refactor(tabnine): remove config requirement to tabnine plugin file and add denylist

--- lua/plugins/tabnine.lua
+++ lua/plugins/tabnine.lua
@@ -1,1 +1,13 @@
-return { "codota/tabnine-nvim", build = "./dl_binaries.sh" }
+return {
+  "codota/tabnine-nvim",
+  build = "./dl_binaries.sh",
+  opts = function() return require("user.tools.tabnine") end,
+  config = function(_, opts) 
+    local tabnine_status_ok, tabnine = pcall(require, "tabnine")
+    if not tabnine_status_ok then
+      vim.notify("Tabnine Plugin Failed!")
+      return
+    end
+    tabnine.setup(opts)
+  end,
+}
---
+++ lua/user/tools/tabnine.lua
@@ -0,0 +1,16 @@
+M = {
+  disable_auto_comment = false,
+  accept_keymap = "<Tab>",
+  dismiss_keymap = "<C-]>",
+  debounce_ms = 800,
+  suggestion_color = { gui = "#808080", cterm = 244 },
+  exclude_filetypes = {
+    "TelescopePrompt",
+    "DressingInput",
+    "toggleterm",
+    "fugitive",
+  },
+  log_file_path = nil,
+}
+
+return M
