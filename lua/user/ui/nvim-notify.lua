0fd09058b817d944e84abf277542b6116777f4ab
Miguel Maza <anmac_tec@outlook.com>
2023-06-29 18:53:01 -0500

    refactor(notify): remove require and add to dependency within noice

--- lua/user/ui/nvim-notify.lua
+++ lua/user/ui/nvim-notify.lua
@@ -1,18 +1,18 @@
-local status_ok, notify = pcall(require, "notify")
-if not status_ok then
-  vim.notify("notify plugin failed!")
-  return
-end
-
-notify.setup({
+M = {
   background_colour = "Normal",
   fps = 5,
   level = 2,
-  max_width = 60,
-  max_height = 20,
   minimum_width = 30,
+  max_width = function()
+    return math.floor(vim.o.columns * 0.75)
+  end,
+  max_height = function()
+    return math.floor(vim.o.lines * 0.75)
+  end,
   render = "default",
-  stages = "static",
+  stages = "fade_in_slide_out",
   timeout = 5000,
   top_down = true,
-})
+}
+
+return M
