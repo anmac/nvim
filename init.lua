af28c1ce2a279af337cbfcabb1e04d4cbaf0ad8e
Miguel Maza <anmac_tec@outlook.com>
2023-06-29 19:32:45 -0500

    refactor(init): remove requirements and just add lazy config

--- init.lua
+++ init.lua
@@ -1,8 +1,25 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local opts = {
+  defaults = {
+    lazy = false,
+    version = "*",
+  },
+  ui = {
+    border = "rounded",
+  }
+}
+
 require("user.core")
-require("user.plugins")
-require("user.colorscheme")
-require("user.treesitter")
-require("user.ui")
-require("user.tools")
-require("user.lsp")
-require("user.cmp")
+require("lazy").setup("plugins", opts)
