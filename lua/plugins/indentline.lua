71daa42b8e6a13c1b453ddb346c30f5f073529fd
Miguel Maza <anmac_tec@outlook.com>
2023-10-24 19:48:41 -0500

    refactor(indentline): update config to v3.*

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -1,13 +1,16 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
   event = { "BufReadPost", "BufNewFile" },
-  opts = function() return require("user.ui.indentline") end,
+  opts = function()
+    return require("user.ui.indentline")
+  end,
   config = function(_, opts)
-    local status_ok, indent_blankline = pcall(require, "indent_blankline")
+    local status_ok, ibl = pcall(require, "ibl")
     if not status_ok then
       vim.notify("Indent Blankline Plugin Failed!")
       return
     end
-    indent_blankline.setup(opts)
+    ibl.setup(opts)
   end,
 }
