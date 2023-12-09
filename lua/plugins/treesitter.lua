13c39365a43caf3103053f6bac8ccee563be9b64
Miguel Maza <anmac_tec@outlook.com>
2023-12-09 12:54:06 -0500

    style(treesitter): apply formatting to treesitter plugin file

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -14,7 +14,9 @@ return {
     "mrjones2014/nvim-ts-rainbow",
     {
       "windwp/nvim-autopairs",
-      opts = function() return require("user.treesitter.nvim-autopairs") end,
+      opts = function()
+        return require("user.treesitter.nvim-autopairs")
+      end,
       config = function(_, opts)
         local status_ok, autopairs = pcall(require, "nvim-autopairs")
         if not status_ok then
@@ -25,10 +27,24 @@ return {
       end,
     },
     "windwp/nvim-ts-autotag",
-    { "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
+    {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+      opts = { enable_autocmd = false },
+      config = function(_, opts)
+        local status_ok, ts_context_commentstring = pcall(require, "ts_context_commentstring")
+        if not status_ok then
+          vim.notify("nvim-ts-context-commentstring Plugin Failed!")
+          return
+        end
+        ts_context_commentstring.setup(opts)
+      end,
+      lazy = true,
+    },
   },
   cmd = { "TSUpdateSync" },
-  opts = function() return require("user.treesitter.nvim-treesitter") end,
+  opts = function()
+    return require("user.treesitter.nvim-treesitter")
+  end,
   config = function(_, opts)
     local status_config_ok, treesitter_config = pcall(require, "nvim-treesitter.configs")
     if not status_config_ok then
