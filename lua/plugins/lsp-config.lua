5a4188460f7b278ed1fdfaeb68145b3ff8fde334
Miguel Maza <anmac_tec@outlook.com>
2024-01-01 15:26:22 -0500

    feat: add lsp config and mason to get lsp servers

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,97 @@
+local Util = require("util")
+
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
+      { "folke/neodev.nvim", opts = {} },
+      {
+        "williamboman/mason.nvim",
+        cmd = "Mason",
+        keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+        build = ":MasonUpdate",
+        opts = {
+          ui = {
+            border = "rounded",
+            width = 0.6,
+            height = 0.8,
+          },
+        },
+        config = function(_, opts)
+          require("mason").setup(opts)
+          local mr = require("mason-registry")
+          mr:on("package:install:success", function()
+            vim.defer_fn(function()
+              -- trigger FileType event to possibly load this newly installed LSP server
+              require("lazy.core.handler.event").trigger({
+                event = "FileType",
+                buf = vim.api.nvim_get_current_buf(),
+              })
+            end, 100)
+          end)
+        end,
+      },
+      {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+          require("mason-lspconfig").setup({
+            ensure_installed = { "lua_ls", "tsserver" }
+          })
+        end
+      },
+    },
+    opts = {
+      diagnostics = {
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+        virtual_text = {
+          spacing = 4,
+          source = "if_many",
+          prefix = "●",
+        },
+        float = {
+          focusable = true,
+          border = "rounded",
+          scope = "cursor",
+          serverity_sort = true,
+          source = "if_many",
+        },
+      },
+      inlay_hints = { enabled = true },
+      capabilities = {},
+      format = {
+        formatting_options = nil,
+        timeout_ms = nil,
+      },
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+            },
+          },
+        },
+      },
+    },
+    config = function()
+      if Util.has("neoconf.nvim") then
+        local plugin = require("lazy.core.config").spec.plugins["neoconf.nvim"]
+        require("neoconf").setup(require("lazy.core.plugin").values(plugin, "opts", false))
+      end
+
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.tsserver.setup({})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+    end,
+  }
+}
