40744d53a274cfbd18458d6899a0ba0ca327fc42
Miguel Maza <anmac_tec@outlook.com>
2024-03-22 18:32:50 -0500

    feat(neo-tree): add keymaps for file explorer with root dir and cwd

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,3 +1,5 @@
+local Util = require("util")
+
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
@@ -9,6 +11,20 @@ return {
   },
   keys = {
     { "<leader>e", "<cmd>Neotree toggle<cr>", desc = "File Explorer" },
+    {
+      "<leader>fe",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = Util.root() })
+      end,
+      desc = "Explorer NeoTree (root dir)",
+    },
+    {
+      "<leader>fE",
+      function()
+        require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+      end,
+      desc = "Explorer NeoTree (cwd)",
+    },
     {
       "<leader>ge",
       function()
