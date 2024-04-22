91b057e4277629850a756db34f4c6340165f4498
Miguel Maza <anmac_tec@outlook.com>
2024-04-21 19:17:10 -0500

    chore(lazyvim): update default options

---
+++ lua/plugins/core.lua
@@ -0,0 +1,72 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "catppuccin",
+      icons = {
+        misc = {
+          dots = "󰇘",
+        },
+        dap = {
+          Stopped = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
+          Breakpoint = " ",
+          BreakpointCondition = " ",
+          BreakpointRejected = { " ", "DiagnosticError" },
+          LogPoint = ".>",
+        },
+        diagnostics = {
+          Error = " ",
+          Warn = " ",
+          Hint = " ",
+          Info = " ",
+        },
+        git = {
+          added = " ",
+          modified = " ",
+          removed = " ",
+        },
+        kinds = {
+          Array = " ",
+          Boolean = "󰨙 ",
+          Class = " ",
+          Codeium = "󰘦 ",
+          Collapsed = " ",
+          Color = " ",
+          Constant = " ",
+          Constructor = " ",
+          Control = " ",
+          Copilot = " ",
+          Enum = " ", -- 
+          EnumMember = " ", -- 
+          Event = " ",
+          Field = " ",
+          File = " ",
+          Folder = " ",
+          Function = " ",
+          Interface = " ", -- 
+          Key = " ",
+          Keyword = " ",
+          Method = " ",
+          Module = " ",
+          Namespace = "󰦮 ",
+          Null = " ",
+          Number = "󰎠 ",
+          Object = "󰅩 ",
+          Operator = " ",
+          Package = "󰏗 ",
+          Property = " ",
+          Reference = " ",
+          Snippet = " ",
+          String = " ",
+          Struct = " ",
+          TabNine = "⌬ ",
+          Text = " ",
+          TypeParameter = " ",
+          Unit = " ",
+          Value = " ",
+          Variable = " ",
+        },
+      },
+    },
+  },
+}
