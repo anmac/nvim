a7d1da13f90c316af93d98558bc1e45d5e305fa6
Miguel Maza <anmac_tec@outlook.com>
2024-04-23 13:15:36 -0500

    fix(keymap): resolve keymap conflict with move in insert mode by adding lsp init section

---
+++ lua/plugins/lsp/init.lua
@@ -0,0 +1,11 @@
+return {
+  -- LSP keymaps
+  {
+    "neovim/nvim-lspconfig",
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- disable a keymap
+      keys[9] = { "<c-K>", false }
+    end,
+  },
+}
