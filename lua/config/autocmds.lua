703d66d8ac87d70d1834fcda8f6b1090e0697924
Miguel Maza <anmac_tec@outlook.com>
2024-04-23 12:18:38 -0500

    feat(autocmds): add window position and restore position autocmds

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,23 @@
+local user = os.getenv("USER") .. "_"
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup(user .. name, { clear = true })
+end
+
+-- Save Window View Position
+vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+  group = augroup("window_leave"),
+  callback = function()
+    vim.api.nvim_exec("let b:winview = winsaveview()", false)
+  end,
+})
+
+-- Restore Window View Position
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  group = augroup("window_enter"),
+  callback = function()
+    if vim.api.nvim_eval("exists('b:winview')") == 1 then
+      vim.api.nvim_exec("call winrestview(b:winview)", false)
+    end
+  end,
+})
