56059adb3efa2617eeda93def77aa500cb17c657
Miguel Maza <anmac_tec@outlook.com>
2024-04-23 12:17:37 -0500

    chore(keymaps): disable buffer nav with h/l and tab keys and add custom keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,6 +17,8 @@ end
 --   command_mode = "c",
 
 -- Disable default global keymaps
+vim.keymap.del("n", "<S-h>")
+vim.keymap.del("n", "<S-l>")
 vim.keymap.del("n", "<leader>ww")
 vim.keymap.del("n", "<leader>wd")
 vim.keymap.del("n", "<leader>w-")
@@ -24,8 +26,31 @@ vim.keymap.del("n", "<leader>w|")
 vim.keymap.del("n", "<leader>-")
 vim.keymap.del("n", "<leader>|")
 vim.keymap.del("n", "<leader>qq")
+vim.keymap.del("n", "<leader><tab>l")
+vim.keymap.del("n", "<leader><tab>f")
+vim.keymap.del("n", "<leader><tab><tab>")
+vim.keymap.del("n", "<leader><tab>]")
+vim.keymap.del("n", "<leader><tab>d")
+vim.keymap.del("n", "<leader><tab>[")
 
 -- Write/Save and Quit
 keymap("n", "<leader>w", "<cmd>w<cr>", opts("Save"))
 keymap("n", "<leader>q", "<cmd>q<cr>", opts("Quit"))
 keymap("n", "<leader>Q", "<cmd>qa<cr>", opts("Quit All"))
+
+-- Move around in Insert Mode with hjkl
+keymap("i", "<C-h>", "<Left>", opts())
+keymap("i", "<C-j>", "<Down>", opts())
+keymap("i", "<C-k>", "<Up>", opts())
+keymap("i", "<C-l>", "<Right>", opts())
+
+-- Stay cursor in place
+keymap("n", "J", "mzJ`z", opts())
+
+-- Select All
+keymap("n", "<C-a>", "ggVG", opts("Select All"))
+
+-- Diagnostic
+keymap("n", "gl", function()
+  vim.diagnostic.open_float({ scope = "line" })
+end, opts("Line Diagnostic"))
