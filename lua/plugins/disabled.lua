14546e9660758475588670cf28057eeafc123ae7
Miguel Maza <anmac_tec@outlook.com>
2024-04-24 14:35:23 -0500

    chore(plugins): disable dashboard-nvim, mini.indentscope

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   { "folke/persistence.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
 }
