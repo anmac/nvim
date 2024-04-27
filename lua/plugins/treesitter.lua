9d08f2a19ca328b2a31b1313fb09d76688bb3f72
Miguel Maza <anmac_tec@outlook.com>
2024-04-27 16:03:55 -0500

    feat(treesitter): extend ensure_installed list and set custom opts

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,56 @@
 return {
 
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "css",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "htmldjango",
+        "http",
+        "make",
+        "php",
+        "query",
+        "scss",
+        "sql",
+        "ssh_config",
+      })
+
+      opts.highlights = {
+        enable = true,
+        disable = function(_, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+        additional_vim_regex_highlighting = false,
+      }
+
+      opts.textobjects = opts.textobjects or {}
+      opts.textobjects.swap = {
+        enable = true,
+        swap_next = {
+          ["<leader>a"] = "@parameter.inner",
+        },
+        swap_previous = {
+          ["<leader>A"] = "@parameter.inner",
+        },
+      }
+
+      opts.autotag = { enable = true }
+
+      return opts
+    end,
+  },
+
   -- rainbow delimiters with treesitter
   {
     "HiPhish/rainbow-delimiters.nvim",
