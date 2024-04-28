4d29a943c9386cae7abbbd806901b9185ca3ef77
Miguel Maza <anmac_tec@outlook.com>
2024-04-28 16:35:17 -0500

    feat(utils): add nvim-ufo for better folds, vim-tmux-navigator for nav between tmux and vim splits

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -33,4 +33,32 @@ return {
       )
     end,
   },
+
+  -- modern fold look keeping high performance
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    config = function()
+      require("ufo").setup()
+    end,
+  },
+
+  -- seamless navigation between tmux panes and vim splits
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 }
