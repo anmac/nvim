bff12c3c388787e61f6368b238cd00a8326a4dc6
Miguel Maza <anmac_tec@outlook.com>
2024-04-28 17:12:29 -0500

    Revert "refactor(neo-tree): use wrap function and overwrite a few opts"
    This reverts commit 6ac22cb76f63d69f91b288b600d88b6ca04f7b60.

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -22,109 +22,108 @@ return {
         desc = "Explorer NeoTree (cwd)",
       },
     },
-    opts = function(_, opts)
-      opts = vim.tbl_extend("force", opts, {
-        close_if_last_window = true,
-        default_source = "last",
-        source_selector = {
-          winbar = true,
-          show_scrolled_off_parent_node = true,
-          sources = {
-            { source = "filesystem" },
-            { source = "buffers" },
-            { source = "git_status" },
-            { source = "document_symbols" },
+    opts = {
+      auto_clean_after_session_restore = false,
+      close_if_last_window = true,
+      default_source = "last",
+      source_selector = {
+        winbar = true,
+        show_scrolled_off_parent_node = true,
+        sources = {
+          { source = "filesystem" },
+          { source = "buffers" },
+          { source = "git_status" },
+          { source = "document_symbols" },
+        },
+        content_layout = "center",
+      },
+      default_component_configs = {
+        container = { enable_character_fade = false },
+        diagnostics = { symbols = { hint = "" } },
+        name = { use_git_status_colors = true },
+        file_size = { enabled = false },
+        type = { enabled = false },
+        last_modified = { enabled = false },
+        symlink_target = { enabled = true },
+      },
+      window = {
+        same_level = true,
+        insert_as = "child",
+        mappings = {
+          ["<space>"] = "noop",
+          ["s"] = "open_split",
+          ["S"] = "noop",
+          ["v"] = "open_vsplit",
+          ["a"] = {
+            "add",
+            config = {
+              show_path = "relative",
+            },
           },
-          content_layout = "center",
+          ["A"] = {
+            "add_directory",
+            config = {
+              show_path = "relative",
+            },
+          },
+          ["Z"] = "expand_all_nodes",
         },
-        default_component_configs = {
-          container = { enable_character_fade = false },
-          diagnostics = { symbols = { hint = "" } },
-          name = { use_git_status_colors = true },
-          file_size = { enabled = false },
-          type = { enabled = false },
-          last_modified = { enabled = false },
-          symlink_target = { enabled = true },
-        },
+      },
+      filesystem = {
         window = {
-          same_level = true,
-          insert_as = "child",
+          fuzzy_finder_mappings = {
+            ["<C-j>"] = "move_cursor_down",
+            ["<C-k>"] = "move_cursor_up",
+          },
+        },
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = { ".git" },
+        },
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = true,
+        },
+      },
+      document_symbols = {
+        follow_cursor = true,
+        window = {
           mappings = {
-            ["<space>"] = "noop",
-            ["s"] = "open_split",
-            ["S"] = "noop",
-            ["v"] = "open_vsplit",
-            ["a"] = {
-              "add",
-              config = {
-                show_path = "relative",
-              },
-            },
-            ["A"] = {
-              "add_directory",
-              config = {
-                show_path = "relative",
-              },
-            },
-            ["Z"] = "expand_all_nodes",
+            ["A"] = "noop",
+            ["a"] = "noop",
           },
         },
-        filesystem = {
-          window = {
-            fuzzy_finder_mappings = {
-              ["<C-j>"] = "move_cursor_down",
-              ["<C-k>"] = "move_cursor_up",
-            },
-          },
-          filtered_items = {
-            hide_dotfiles = false,
-            hide_gitignored = true,
-            hide_by_name = { ".git" },
-          },
-          follow_current_file = {
-            enabled = true,
-            leave_dirs_open = true,
-          },
+        kinds = {
+          Array = { icon = "", hl = "@type" },
+          Boolean = { icon = "󰨙", hl = "@boolean" },
+          Class = { icon = "", hl = "Structure" },
+          Constant = { icon = "", hl = "@constant" },
+          Constructor = { icon = "", hl = "@constructor" },
+          Enum = { icon = "", hl = "@number" },
+          EnumMember = { icon = "", hl = "@number" },
+          Event = { icon = "", hl = "@constant" },
+          Field = { icon = "", hl = "@field" },
+          File = { icon = "󰈙", hl = "Tag" },
+          Function = { icon = "", hl = "@function" },
+          Interface = { icon = "", hl = "@type" },
+          Method = { icon = "", hl = "@function" },
+          Module = { icon = "", hl = "@module" },
+          Namespace = { icon = "󰦮", hl = "@namespace" },
+          Null = { icon = "", hl = "@constant" },
+          Number = { icon = "󰎠", hl = "@number" },
+          Operator = { icon = "", hl = "@operator" },
+          Package = { icon = "󰏗", hl = "Label" },
+          Parameter = { icon = "", hl = "@parameter" },
+          Property = { icon = "", hl = "@property" },
+          StaticMethod = { icon = "󰠄", hl = "Function" },
+          String = { icon = "", hl = "@string" },
+          Struct = { icon = "", hl = "@type" },
+          TypeAlias = { icon = "", hl = "@type" },
+          Variable = { icon = "", hl = "@variable" },
         },
-        document_symbols = {
-          follow_cursor = true,
-          window = {
-            mappings = {
-              ["A"] = "noop",
-              ["a"] = "noop",
-            },
-          },
-          kinds = {
-            Array = { icon = "", hl = "@type" },
-            Boolean = { icon = "󰨙", hl = "@boolean" },
-            Class = { icon = "", hl = "Structure" },
-            Constant = { icon = "", hl = "@constant" },
-            Constructor = { icon = "", hl = "@constructor" },
-            Enum = { icon = "", hl = "@number" },
-            EnumMember = { icon = "", hl = "@number" },
-            Event = { icon = "", hl = "@constant" },
-            Field = { icon = "", hl = "@field" },
-            File = { icon = "󰈙", hl = "Tag" },
-            Function = { icon = "", hl = "@function" },
-            Interface = { icon = "", hl = "@type" },
-            Method = { icon = "", hl = "@function" },
-            Module = { icon = "", hl = "@module" },
-            Namespace = { icon = "󰦮", hl = "@namespace" },
-            Null = { icon = "", hl = "@constant" },
-            Number = { icon = "󰎠", hl = "@number" },
-            Operator = { icon = "", hl = "@operator" },
-            Package = { icon = "󰏗", hl = "Label" },
-            Parameter = { icon = "", hl = "@parameter" },
-            Property = { icon = "", hl = "@property" },
-            StaticMethod = { icon = "󰠄", hl = "Function" },
-            String = { icon = "", hl = "@string" },
-            Struct = { icon = "", hl = "@type" },
-            TypeAlias = { icon = "", hl = "@type" },
-            Variable = { icon = "", hl = "@variable" },
-          },
-        },
-      })
-    end,
+      },
+    },
   },
 
   -- Fuzzy finder. Telescope
