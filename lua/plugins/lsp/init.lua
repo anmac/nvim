a07a19c2b98f3190b8b16737fe15cfe3fb5a7689
Miguel Maza <anmac_tec@outlook.com>
2024-04-29 13:08:01 -0500

    feat(mason): add ui opts, extend ensure list with biome, google-java-format

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,4 +1,5 @@
 return {
+
   -- LSP keymaps
   {
     "neovim/nvim-lspconfig",
@@ -6,6 +7,20 @@ return {
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       -- disable a keymap
       keys[9] = { "<c-K>", false }
+    end,
+  },
+
+  -- mason
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ui = {
+        border = "rounded",
+        width = 0.6,
+        height = 0.8,
+      }
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "biome", "google-java-format" })
     end,
   },
 }
