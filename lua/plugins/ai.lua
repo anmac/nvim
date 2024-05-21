9ba63c007a65cfa65dd13f78d932a22a90d8e5ce
Miguel Maza <anmac_tec@outlook.com>
2024-05-21 14:22:26 -0500

    feat(ai): disable codeium vim version

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -3,6 +3,7 @@ return {
   -- Free, ultrafast Copilot alternative
   {
     "Exafunction/codeium.vim",
+    enabled = false,
     event = "BufEnter",
     config = function()
       vim.g.codeium_filetypes = {
