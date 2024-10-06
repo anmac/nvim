81184c4cc1e9a6c11206d2be3f5623e5134f5d0e
Miguel Maza <anmac_tec@outlook.com>
2024-10-06 11:50:50 -0500

    chore(keymap): uncomment tab related mappings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -34,15 +34,15 @@ vim.keymap.del("t", "<c-_>")
 vim.keymap.del("n", "<leader>-")
 vim.keymap.del("n", "<leader>|")
 -- vim.keymap.del("n", "<leader>wm")
-vim.keymap.del("n", "<leader><tab>l")
-vim.keymap.del("n", "<leader><tab>f")
-vim.keymap.del("n", "<leader><tab><tab>")
-vim.keymap.del("n", "<leader><tab>]")
-vim.keymap.del("n", "<leader><tab>d")
-vim.keymap.del("n", "<leader><tab>[")
+-- vim.keymap.del("n", "<leader><tab>l")
+-- vim.keymap.del("n", "<leader><tab>f")
+-- vim.keymap.del("n", "<leader><tab><tab>")
+-- vim.keymap.del("n", "<leader><tab>]")
+-- vim.keymap.del("n", "<leader><tab>d")
+-- vim.keymap.del("n", "<leader><tab>[")
 
 -- Write/Save and Quit
-keymap("n", "<leader>w", "<cmd>w<cr>", opts("Save"))
+keymap("n", "<leader>w", "<cmd>w<cr>", opts("Save/Windows"))
 keymap("n", "<leader>q", "<cmd>q<cr>", opts("Quit"))
 keymap("n", "<leader>Q", "<cmd>qa<cr>", opts("Quit All"))
 
@@ -64,17 +64,17 @@ keymap("n", "gl", function()
 end, opts("Line Diagnostic"))
 
 -- floating terminal
-local lazyterm = function()
-  LazyVim.terminal(nil, { cwd = LazyVim.root() })
-end
-keymap("n", "<c-\\>", lazyterm, opts("Terminal (Root Dir)"))
-keymap("n", "<c-_>", lazyterm, opts("which_key_ignore"))
+-- local lazyterm = function()
+--   LazyVim.terminal(nil, { cwd = LazyVim.root() })
+-- end
+-- keymap("n", "<c-\\>", lazyterm, opts("Terminal (Root Dir)"))
+-- keymap("n", "<c-_>", lazyterm, opts("which_key_ignore"))
 
 -- Terminal Mappings
-keymap("t", "<c-\\>", "<cmd>close<cr>", opts("Hide Terminal"))
-keymap("t", "<c-_>", "<cmd>close<cr>", opts("which_key_ignore"))
+-- keymap("t", "<c-\\>", "<cmd>close<cr>", opts("Hide Terminal"))
+-- keymap("t", "<c-_>", "<cmd>close<cr>", opts("which_key_ignore"))
 
 -- Windows
-keymap("n", "<leader>m", function()
-  LazyVim.toggle.maximize()
-end, { desc = "Maximize Toggle" })
+-- keymap("n", "<leader>m", function()
+--   LazyVim.toggle.maximize()
+-- end, { desc = "Maximize Toggle" })
