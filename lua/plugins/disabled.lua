fc246919da6f7144d2c39b6ed28255e2cc063707
Miguel Maza <anmac_tec@outlook.com>
2024-10-07 09:52:19 -0500

    chore: disable indent-blankline.nvim plugin

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,5 +3,6 @@ return {
   { "folke/persistence.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "echasnovski/mini.indentscope", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
 }
