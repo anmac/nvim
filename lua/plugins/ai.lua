e4d4c89a9e033c0a26aef3a442f513d0dc764b70
Miguel Maza <anmac_tec@outlook.com>
2024-10-07 10:06:45 -0500

    refactor: cleanup for lazy conf, java, ai and editor
    - Lazy: remove explicitly imports for language
    - java: delete java dir which was used for and alternative to nvim-jdtls
    - ai: remove codeium.vim conf as no more required. Supermaven is better
    - editor: remove for document_symbols (neo-tree) as it's no longer used,
    remove event field for which-key, remove on_attach conf for gitsigns as
    the default conf is enough for me

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,23 +1,5 @@
 return {
 
-  -- Free, ultrafast Copilot alternative
-  {
-    "Exafunction/codeium.vim",
-    enabled = false,
-    event = "BufEnter",
-    config = function()
-      vim.g.codeium_filetypes = {
-        ["DressingInput"] = false,
-        ["fugitive"] = false,
-        ["toggleterm"] = false,
-        ["TelescopePrompt"] = false,
-        ["neo-tree-popup"] = false,
-        ["nofile"] = false,
-      }
-      vim.api.nvim_set_hl(0, "Codeium", { fg = "#6fe8d7" })
-    end,
-  },
-
   -- The official Neovim plugin for Supermaven
   {
     "supermaven-inc/supermaven-nvim",
--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -13,23 +13,8 @@ return {
           { source = "filesystem" },
           { source = "buffers" },
           { source = "git_status" },
-          { source = "document_symbols" },
         },
         content_layout = "center",
-      },
-      event_handlers = {
-        {
-          event = require("neo-tree.events").GIT_EVENT,
-          handler = function()
-            require("neo-tree.sources.git_status").refresh()
-          end,
-        },
-        {
-          event = require("neo-tree.events").GIT_STATUS_CHANGED,
-          handler = function()
-            require("neo-tree.sources.git_status").refresh()
-          end,
-        },
       },
       default_component_configs = {
         container = { enable_character_fade = false },
@@ -78,43 +63,6 @@ return {
         follow_current_file = {
           enabled = true,
           leave_dirs_open = true,
-        },
-      },
-      document_symbols = {
-        follow_cursor = true,
-        window = {
-          mappings = {
-            ["A"] = "noop",
-            ["a"] = "noop",
-          },
-        },
-        kinds = {
-          Array = { icon = "", hl = "@type" },
-          Boolean = { icon = "󰨙", hl = "@boolean" },
-          Class = { icon = "", hl = "Structure" },
-          Constant = { icon = "", hl = "@constant" },
-          Constructor = { icon = "", hl = "@constructor" },
-          Enum = { icon = "", hl = "@number" },
-          EnumMember = { icon = "", hl = "@number" },
-          Event = { icon = "", hl = "@constant" },
-          Field = { icon = "", hl = "@field" },
-          File = { icon = "󰈙", hl = "Tag" },
-          Function = { icon = "", hl = "@function" },
-          Interface = { icon = "", hl = "@type" },
-          Method = { icon = "", hl = "@function" },
-          Module = { icon = "", hl = "@module" },
-          Namespace = { icon = "󰦮", hl = "@namespace" },
-          Null = { icon = "", hl = "@constant" },
-          Number = { icon = "󰎠", hl = "@number" },
-          Operator = { icon = "", hl = "@operator" },
-          Package = { icon = "󰏗", hl = "Label" },
-          Parameter = { icon = "", hl = "@parameter" },
-          Property = { icon = "", hl = "@property" },
-          StaticMethod = { icon = "󰠄", hl = "Function" },
-          String = { icon = "", hl = "@string" },
-          Struct = { icon = "", hl = "@type" },
-          TypeAlias = { icon = "", hl = "@type" },
-          Variable = { icon = "", hl = "@variable" },
         },
       },
     },
@@ -212,7 +160,6 @@ return {
   -- which_key
   {
     "folke/which-key.nvim",
-    event = "VeryLazy",
     opts = {
       preset = "helix",
       delay = 300,
@@ -245,37 +192,6 @@ return {
       current_line_blame = true,
       current_line_blame_opts = { delay = 500 },
       preview_config = { border = "rounded" },
-      on_attach = function(buffer)
-        local gs = package.loaded.gitsigns
-
-        local function map(mode, l, r, desc)
-          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
-        end
-
-        map("n", "]h", function()
-          gs.nav_hunk("next")
-        end, "Next Hunk")
-        map("n", "[h", function()
-          gs.nav_hunk("prev")
-        end, "Prev Hunk")
-        map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
-        map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-        map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
-        map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
-        map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
-        map("n", "<leader>ghp", gs.preview_hunk_inline, "Preview Hunk Inline")
-        map("n", "<leader>ghP", gs.preview_hunk, "Preview Hunk")
-        map("n", "<leader>ghb", gs.blame_line, "Blame Line")
-        map("n", "<leader>ghB", function()
-          gs.blame_line({ full = true })
-        end, "Blame Line Full")
-        map("n", "<leader>ghd", gs.diffthis, "Diff This")
-        map("n", "<leader>ghD", function()
-          gs.diffthis("~")
-        end, "Diff This ~")
-        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
-        vim.cmd([[highlight GitSignsCurrentLineBlame guifg=#6d707c]])
-      end,
     },
   },
 
