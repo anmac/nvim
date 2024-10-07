6a9a85bee4c6fa40368b230b18b2c4f78088087a
Miguel Maza <anmac_tec@outlook.com>
2024-10-07 10:20:15 -0500

    perf(treesitter): pass just textobjects conf to opts to make merge easier

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,50 +3,27 @@ return {
   -- Treesitter
   {
     "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, {
-        "css",
-        "git_config",
-        "git_rebase",
-        "gitattributes",
-        "gitcommit",
-        "gitignore",
-        "htmldjango",
-        "http",
-        "make",
-        "php",
-        "query",
-        "scss",
-        "sql",
-        "ssh_config",
-      })
-
-      opts.highlights = {
-        enable = true,
-        disable = function(_, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
-        additional_vim_regex_highlighting = false,
-      }
-
-      opts.textobjects = opts.textobjects or {}
-      opts.textobjects.swap = {
-        enable = true,
-        swap_next = {
-          ["<leader>a"] = "@parameter.inner",
+    opts = {
+      auto_install = true,
+      textobjects = {
+        move = {
+          enable = true,
+          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
         },
-        swap_previous = {
-          ["<leader>A"] = "@parameter.inner",
+        swap = {
+          enable = true,
+          swap_next = {
+            ["<leader>a"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["<leader>A"] = "@parameter.inner",
+          },
         },
-      }
-
-      opts.autotag = { enable = true }
-    end,
+      },
+    },
   },
 
   -- autopairs for neovim
