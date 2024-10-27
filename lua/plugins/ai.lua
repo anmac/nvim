6d535495cb64e8fbbd401af9bd02281520fef448
Miguel Maza <anmac_tec@outlook.com>
2024-10-27 12:15:03 -0500

    perf(ai): set supermaven-nvim as lazy loaded
    Slower since supermaven-nvim always load cmp and its sources
    Stop loading automatically. I will load it just when necessary

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -3,7 +3,7 @@ return {
   -- The official Neovim plugin for Supermaven
   {
     "supermaven-inc/supermaven-nvim",
-    enabled = false,
+    lazy = true,
     opts = {
       ignore_filetypes = {
         ["DressingInput"] = true,
