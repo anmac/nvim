28f527bc2d5ff546fee98f5b2f073a237d25bb99
Miguel Maza <anmac_tec@outlook.com>
2024-12-31 09:01:34 -0500

    feat(snippet): extend php file type with html snippets

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -14,4 +14,13 @@ return {
       },
     },
   },
+
+  {
+    "L3MON4D3/LuaSnip",
+    optional = true,
+    config = function()
+      -- require("luasnip.loaders.from_vscode").lazy_load()
+      require("luasnip").filetype_extend("php", { "html" })
+    end,
+  },
 }
