6cf173bfff57828258c0d382c092c77bd9ad77e2
Miguel Maza <anmac_tec@outlook.com>
2025-02-15 13:48:53 -0500

    chore(git): don't overwrite add, change, delete, and topdelte icons for gitsigns

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -205,12 +205,11 @@ return {
     },
     opts = {
       signs = {
-        add = { text = "┃" },
-        change = { text = "┃" },
-        delete = { text = "" },
-        topdelete = { text = "" },
         changedelete = { text = "" },
         untracked = { text = "┆" },
+      },
+      signs_staged = {
+        changedelete = { text = "▎" },
       },
       attach_to_untracked = true,
       current_line_blame = true,
